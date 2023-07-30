75d4027be969b09f34e5a07fed02ba1ae2166b15
Xeferis <florian@hillebold.de>
2023-07-30 16:44:20 +0200

    Added Slash commands

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ a_intents = discord.Intents.default()
 a_intents.message_content = True
 
 bot = discord.Client(intents=a_intents)
+tree = discord.app_commands.CommandTree(bot)
+
 
 try:
     with open("config.json", "r") as f:
@@ -13,7 +15,7 @@ try:
 except:
     raise
 
-rules = """
+main_rules = """
 Welcome to Coding Vikings' realm!
 Please read the rules and accept them in the Channel "welcome"
 
@@ -25,24 +27,27 @@ Rules
 5. Not listening to Kings or Jarls can end in a permanent ban from our realm!
 """
 
-chat_commands = {
-    '/skal': 'SKAL!!',
-    '/rules': rules
-}
 
 @bot.event
 async def on_ready():
+    await tree.sync(guild=discord.Object(id=1133683869317595186))
     print(f"We have logged in as {bot.user}")
+
+
+@tree.command(name="skal", description="Greet Ubba!")
+async def skal(interaction):
+    await interaction.response.send_message(f"SKAL, {interaction.user}!!")
+
+
+@tree.command(name="rules", description="Get all current rules")
+async def rules(interaction):
+    await interaction.response.send_message(main_rules)
 
 
 @bot.event
 async def on_message(message):
-    if message.author == bot.user:
-        return
-
-    for key in chat_commands.keys():
-        if message.content.startswith(key):
-            await message.channel.send(chat_commands[key])
-            break
+    if message.content.startswith("/update_tree"):
+        print("Updating commandtree")
+        await tree.sync(guild=discord.Object(id=1133683869317595186))
 
 bot.run(token=r_token)
